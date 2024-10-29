fa1c36db79380590ff3a5e478d55508c73362c1f
Gurpartap Grewal <luis.gomez.cohort238@gmail.com>
2024-10-29 11:55:06 -0700

    added cover sheet and POA's

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { PDFDocument } from 'pdf-lib';
+import { format } from 'date-fns';
 import './App.css';
 
 const App = () => {
@@ -7,19 +8,28 @@ const App = () => {
         buyerName: '',
         buyerEmail: '',
         buyerAddress: '',
+        buyerYearsDriving: '',
+        buyerYearsOwner: '',
+        buyerFleetSize: '',
+        buyerTrucksInFleet: '',
+        buyerTrailersInFleet: '',
         date: '',
         salesPerson: '',
         dealNumber: '',
+        stockNumber: '',
         sellerName: '',
         sellerCompanyName: '',
         sellerAddress: '',
-        sellerCityStateZip: '',
+        sellerCity: '',
+        sellerState: '',
+        sellerZip: '',
         sellerPhone: '',
         sellerEmail: '',
         buyerCompanyName: '',
-        buyerCityStateZip: '',
+        buyerCity: '',
+        buyerState: '',
+        buyerZip: '',
         buyerPhone: '',
-        stockNumber: '',
         assetYear: '',
         assetMake: '',
         assetModel: '',
@@ -70,8 +80,7 @@ const App = () => {
     const fillPdfWithFormFields = async (formData) => {
         const pdfBytes = await loadPdf('/pdfs/billOfSale.pdf'); // Load your PDF
         const pdfDoc = await PDFDocument.load(pdfBytes);
-
-        const form = pdfDoc.getForm(); // Access the form
+        const form = pdfDoc.getForm();
 
         // Helper function to set text for a field
         const setFieldText = (fieldName, value) => {
@@ -83,29 +92,41 @@ const App = () => {
             }
         };
 
+        // Format the date before setting it in the PDF
+        const formattedDate = format(new Date(formData.date + 'T00:00:00'), 'MM/dd/yy');
+        setFieldText('date', formattedDate);
+
         // Fill in the PDF form fields using the helper function
         setFieldText('buyerName', formData.buyerName);
         setFieldText('buyerEmail', formData.buyerEmail);
         setFieldText('buyerAddress', formData.buyerAddress);
-        setFieldText('date', formData.date);
         setFieldText('salesPerson', formData.salesPerson);
         setFieldText('dealNumber', formData.dealNumber);
         setFieldText('sellerName', formData.sellerName);
         setFieldText('sellerCompanyName', formData.sellerCompanyName);
         setFieldText('sellerAddress', formData.sellerAddress);
-        setFieldText('sellerCityStateZip', formData.sellerCityStateZip);
+        setFieldText('sellerCity', formData.sellerCity);
+        setFieldText('sellerState', formData.sellerState);
+        setFieldText('sellerZip', formData.sellerZip);
         setFieldText('sellerPhone', formData.sellerPhone);
         setFieldText('sellerEmail', formData.sellerEmail);
         setFieldText('buyerCompanyName', formData.buyerCompanyName);
-        setFieldText('buyerCityStateZip', formData.buyerCityStateZip);
+        setFieldText('buyerCity', formData.buyerCity);
+        setFieldText('buyerState', formData.buyerState);
+        setFieldText('buyerZip', formData.buyerZip);
         setFieldText('buyerPhone', formData.buyerPhone);
+        setFieldText('buyerYearsDriving', formData.buyerYearsDriving);
+        setFieldText('buyerYearsOwner', formData.buyerYearsOwner);
+        setFieldText('buyerFleetSize', formData.buyerFleetSize);
+        setFieldText('buyerTrucksInFleet', formData.buyerTrucksInFleet);
+        setFieldText('buyerTrailersInFleet', formData.buyerTrailersInFleet);
         setFieldText('stockNumber', formData.stockNumber);
         setFieldText('assetYear', formData.assetYear);
         setFieldText('assetMake', formData.assetMake);
         setFieldText('assetModel', formData.assetModel);
         setFieldText('VIN', formData.VIN);
         setFieldText('assetMiles', formData.assetMiles);
-        setFieldText('assetHours', formData.assetHours); // This will be ignored if the field doesn't exist
+        setFieldText('assetHours', formData.assetHours);
         setFieldText('assetEngine', formData.assetEngine);
         setFieldText('assetEngineSerial', formData.assetEngineSerial);
         setFieldText('assetFuelTank', formData.assetFuelTank);
@@ -125,18 +146,7 @@ const App = () => {
         setFieldText('lienholderPhoneNumber', formData.lienholderPhoneNumber);
         setFieldText('payOff', formData.payOff);
         setFieldText('payOffExpiration', formData.payOffExpiration);
-        setFieldText('sellerCDLNumber', formData.sellerCDLNumber);
-        setFieldText('buyerCDLNumber', formData.buyerCDLNumber);
-        setFieldText('buyerYearsDriving', formData.buyerYearsDriving);
-        setFieldText('buyerYearsOwner', formData.buyerYearsDriving);
-        setFieldText('assetPlateNumber', formData.buyerYearsDriving);
-        setFieldText('buyerFleetSize', formData.buyerFleetSize);
-        setFieldText('buyerTrucksInFleet', formData.buyerTrucksInFleet);
-        setFieldText('buyerTrailersInFleet', formData.buyerTrailersInFleet);
-        setFieldText('buyerEIN', formData.buyerEIN);
-        setFieldText('buyerSS', formData.buyerSS);
-        setFieldText('buyerCDLState', formData.buyerCDLState);
-        setFieldText('sellerCDLState', formData.sellerCDLState);
+        // Include any additional fields as needed
 
         // Serialize the PDF and trigger a download
         const pdfBytesFilled = await pdfDoc.save();
@@ -149,76 +159,83 @@ const App = () => {
 
     return (
         <form onSubmit={handleSubmit}>
-          <div className='header'>
-            <input type="date" name="date" onChange={handleChange} /> 
-            <input type="text" name="salesPerson" placeholder="Sales Person" onChange={handleChange} />
-            <input type="text" name="dealNumber" placeholder="Deal Number" onChange={handleChange} />
-            <input type="text" name="stockNumber" placeholder="Stock Number" onChange={handleChange} />
-          </div>
-          <div className='buyerInfo'>
-            <h2>Buyer</h2>
-            <input type="text" name="buyerName" placeholder="Buyer Name" onChange={handleChange} />
-            <input type="email" name="buyerEmail" placeholder="Buyer Email" onChange={handleChange} />
-            <input type="text" name="buyerAddress" placeholder="Buyer Address" onChange={handleChange} />
-            <input type="text" name="buyerCompanyName" placeholder="Buyer Company Name" onChange={handleChange} />
-            <input type="text" name="buyerEIN" placeholder="EIN" onChange={handleChange} />
-            <input type="text" name="buyerCDLNumber" placeholder="CDL Number" onChange={handleChange} />
-            <input type="text" name="buyerCDLState" placeholder="CDL State" onChange={handleChange} />
-            <input type="text" name="buyerSS" placeholder="SSN" onChange={handleChange} />
-            <input type="text" name="buyerCityStateZip" placeholder="Buyer City, State, Zip" onChange={handleChange} />
-            <input type="tel" name="buyerPhone" placeholder="Buyer Phone" onChange={handleChange} />
-            <input type="text" name="buyerYearsDriving" placeholder="Commercial Driving Experience" onChange={handleChange} />
-            <input type="text" name="buyerYearsOwner" placeholder="Owner Operator Experience" onChange={handleChange} />
-            <input type="text" name="buyerFleetSize" placeholder="Fleet Size" onChange={handleChange} />
-            <input type="text" name="buyerTrucksInFleet" placeholder="Trucks in Fleet" onChange={handleChange} />
-            <input type="text" name="buyerTrailersInFleet" placeholder="Trailers in Fleet" onChange={handleChange} />
+            <div className='header'>
+                <input 
+                    type="date" 
+                    name="date" 
+                    onChange={handleChange} 
+                /> 
+                <input type="text" name="salesPerson" placeholder="Sales Person" onChange={handleChange} />
+                <input type="text" name="dealNumber" placeholder="Deal Number" onChange={handleChange} />
+                <input type="text" name="stockNumber" placeholder="Stock Number" onChange={handleChange} />
+            </div>
 
+            <div className='buyerInfo'>
+                <h2>Buyer</h2>
+                <input type="text" name="buyerName" placeholder="Full Name" onChange={handleChange} />
+                <input type="text" name="buyerCompanyName" placeholder="Company Name" onChange={handleChange} />
+                <input type="email" name="buyerEmail" placeholder="Email" onChange={handleChange} />
+                <input type="tel" name="buyerPhone" placeholder="Phone Number" onChange={handleChange} />
+                <input type="text" name="buyerAddress" placeholder="Street Address" onChange={handleChange} />
+                <input type="text" name="buyerCity" placeholder="City" onChange={handleChange} />
+                <input type="text" name="buyerState" placeholder="State" onChange={handleChange} />
+                <input type="number" name="buyerZip" placeholder="Zip" onChange={handleChange} />
+                <input type="text" name="buyerCDLNumber" placeholder="CDL Number" onChange={handleChange} />
+                <input type="text" name="buyerCDLState" placeholder="CDL State" onChange={handleChange} />
+                <input type="text" pattern="\d{3}-\d{2}-\d{4}" name="buyerSS" placeholder="SSN" onChange={handleChange} />
+                <input type="text" name="buyerEIN" placeholder="EIN" onChange={handleChange} />
+                <input type="number" name="buyerYearsDriving" placeholder="Commercial Driving Experience" onChange={handleChange} />
+                <input type="number" name="buyerYearsOwner" placeholder="Owner Operator Experience" onChange={handleChange} />
+                <input type="number" name="buyerFleetSize" placeholder="Fleet Size" onChange={handleChange} />
+                <input type="number" name="buyerTrucksInFleet" placeholder="Trucks in Fleet" onChange={handleChange} />
+                <input type="number" name="buyerTrailersInFleet" placeholder="Trailers in Fleet" onChange={handleChange} />
+            </div>
 
+            <div className='sellerInfo'>
+                <h2>Seller</h2>
+                <input type="text" name="sellerName" placeholder="Full Name" onChange={handleChange} />
+                <input type="text" name="sellerCompanyName" placeholder="Company Name" onChange={handleChange} />
+                <input type="email" name="sellerEmail" placeholder="Email" onChange={handleChange} />
+                <input type="tel" name="sellerPhone" placeholder="Phone Number" onChange={handleChange} />
+                <input type="text" name="sellerAddress" placeholder="Street Address" onChange={handleChange} />
+                <input type="text" name="sellerCity" placeholder="City" onChange={handleChange} />
+                <input type="text" name="sellerState" placeholder="State" onChange={handleChange} />
+                <input type="number" name="sellerZip" placeholder="Zip" onChange={handleChange} />
+            </div>
 
+            <div className='assetInfo'>
+                <h2>Asset Info</h2>
+                <input type="text" name="assetYear" placeholder="Year" onChange={handleChange} />
+                <input type="text" name="assetMake" placeholder="Make" onChange={handleChange} />
+                <input type="text" name="assetModel" placeholder="Model" onChange={handleChange} />
+                <input type="text" name="VIN" placeholder="VIN" onChange={handleChange} />
+                <input type="number" name="assetMiles" placeholder="Miles" onChange={handleChange} />
+                <input type="number" name="assetHours" placeholder="Hours" onChange={handleChange} />
+                <input type="text" name="assetEngine" placeholder="Engine" onChange={handleChange} />
+                <input type="text" name="assetEngineSerial" placeholder="Engine Serial" onChange={handleChange} />
+                <input type="text" name="assetFuelTank" placeholder="Fuel Tank" onChange={handleChange} />
+                <input type="text" name="assetTransmission" placeholder="Transmission" onChange={handleChange} />
+                <input type="text" name="assetSuspension" placeholder="Suspension" onChange={handleChange} />
+                <input type="text" name="assetTireSize" placeholder="Tire Size" onChange={handleChange} />
+                <input type="text" name="assetWheelType" placeholder="Wheel Type" onChange={handleChange} />
+                <input type="text" name="assetSleeper" placeholder="Sleeper" onChange={handleChange} />
+                <input type="text" name="assetDoubleBunk" placeholder="Double Bunk" onChange={handleChange} />
+                <input type="text" name="assetInteriorColor" placeholder="Interior Color" onChange={handleChange} />
+                <input type="text" name="assetExteriorColor" placeholder="Exterior Color" onChange={handleChange} />
+                <input type="text" name="assetExteriorOptions" placeholder="Exterior Options" onChange={handleChange} />
+            </div>
 
+            <div className='paymentInfo'>
+                <h2>Payment Info</h2>
+                <input type="number" name="salePrice" placeholder="Sale Price" onChange={handleChange} />
+                <input type="text" name="lienholderName" placeholder="Lienholder Name" onChange={handleChange} />
+                <input type="text" name="lienholderAddress" placeholder="Lienholder Address" onChange={handleChange} />
+                <input type="text" name="lienholderCityStateZip" placeholder="City, State, Zip" onChange={handleChange} />
+                <input type="tel" name="lienholderPhoneNumber" placeholder="Lienholder Phone Number" onChange={handleChange} />
+                <input type="number" name="payOff" placeholder="Pay Off" onChange={handleChange} />
+                <input type="text" name="payOffExpiration" placeholder="Pay Off Expiration" onChange={handleChange} />
+            </div>
 
-          </div>
-          <div className='sellerInfo'>
-            <h2>Seller</h2>
-            <input type="text" name="sellerName" placeholder="Seller Name" onChange={handleChange} />
-            <input type="text" name="sellerCompanyName" placeholder="Seller Company Name" onChange={handleChange} />
-            <input type="text" name="sellerCDLNumber" placeholder="CDL Number" onChange={handleChange} />
-            <input type="text" name="sellerCDLState" placeholder="CDL State" onChange={handleChange} />
-            <input type="text" name="sellerAddress" placeholder="Seller Address" onChange={handleChange} />
-            <input type="text" name="sellerCityStateZip" placeholder="Seller City, State, Zip" onChange={handleChange} />
-            <input type="tel" name="sellerPhone" placeholder="Seller Phone" onChange={handleChange} />
-            <input type="email" name="sellerEmail" placeholder="Seller Email" onChange={handleChange} />
-          </div>
-          <div className='assetDetails'>
-            <h2>Asset</h2>
-            <input type="text" name="assetYear" placeholder="Asset Year" onChange={handleChange} />
-            <input type="text" name="assetMake" placeholder="Asset Make" onChange={handleChange} />
-            <input type="text" name="assetModel" placeholder="Asset Model" onChange={handleChange} />
-            <input type="text" name="VIN" placeholder="VIN" onChange={handleChange} />
-            <input type="text" name="assetPlateNumber" placeholder="lic plate number" onChange={handleChange} />
-            <input type="text" name="assetMiles" placeholder="Asset Miles" onChange={handleChange} />
-            <input type="text" name="assetHours" placeholder="Asset Hours" onChange={handleChange} /> {/* Optional field */}
-            <input type="text" name="assetEngine" placeholder="Asset Engine" onChange={handleChange} />
-            <input type="text" name="assetEngineSerial" placeholder="Asset Engine Serial" onChange={handleChange} />
-            <input type="text" name="assetFuelTank" placeholder="Asset Fuel Tank" onChange={handleChange} />
-            <input type="text" name="assetTransmission" placeholder="Asset Transmission" onChange={handleChange} />
-            <input type="text" name="assetSuspension" placeholder="Asset Suspension" onChange={handleChange} />
-            <input type="text" name="assetTireSize" placeholder="Asset Tire Size" onChange={handleChange} />
-            <input type="text" name="assetWheelType" placeholder="Asset Wheel Type" onChange={handleChange} />
-            <input type="text" name="assetSleeper" placeholder="Asset Sleeper" onChange={handleChange} />
-            <input type="text" name="assetDoubleBunk" placeholder="Asset Double Bunk" onChange={handleChange} />
-            <input type="text" name="assetInteriorColor" placeholder="Asset Interior Color" onChange={handleChange} />
-            <input type="text" name="assetExteriorColor" placeholder="Asset Exterior Color" onChange={handleChange} />
-            <input type="text" name="assetExteriorOptions" placeholder="Asset Exterior Options" onChange={handleChange} />
-            <input type="text" name="salePrice" placeholder="Sale Price" onChange={handleChange} />
-            <input type="text" name="lienholderName" placeholder="Lienholder Name" onChange={handleChange} />
-            <input type="text" name="lienholderAddress" placeholder="Lienholder Address" onChange={handleChange} />
-            <input type="text" name="lienholderCityStateZip" placeholder="Lienholder City, State, Zip" onChange={handleChange} />
-            <input type="tel" name="lienholderPhoneNumber" placeholder="Lienholder Phone Number" onChange={handleChange} />
-            <input type="text" name="payOff" placeholder="Pay Off" onChange={handleChange} />
-            <input type="text" name="payOffExpiration" placeholder="Pay Off Expiration" onChange={handleChange} />
-          </div>
-          <br></br>
             <button type="submit">Submit</button>
         </form>
     );
